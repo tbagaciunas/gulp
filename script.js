22b63f9cc16423367be1aaf9133e98bf76d029da
tbagaciunas <tomas1089@gmail.com>
2022-11-17 04:18:44 +0200

    init

---
+++ script.js
@@ -0,0 +1,57 @@
+
+function one(){
+
+    document.getElementById('galerija2').src = "https://images.unsplash.com/photo-1598030343246-eec71cb44231?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80.jpg";
+    document.getElementById('galerija2').style.width = "168px";
+    document.getElementById('galerija2').style.height = "168px";
+    document.getElementById('galerija2').style.transition = "1s"
+    document.getElementById('galerija2').style.position = "relative";
+    
+    document.getElementById('galerija2').style.top = "-400px"; 
+    document.getElementById('galerija2').style.left = "1220px";
+    document.getElementById('galerija2').style.ease = "1s";
+    document.getElementById('galerija1').src = "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80.jpg";
+    document.getElementById('galerija1').style.width = "168px";
+    document.getElementById('galerija1').style.height = "168px";
+    document.getElementById('galerija1').style.transition = "1s"
+    document.getElementById('galerija1').style.position = "relative";
+    
+
+
+
+    document.getElementById('galerija1').style.ease = "1s";
+
+
+    document.getElementById('galerija3').src = "https://images.unsplash.com/photo-1502101872923-d48509bff386?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80.jpg";
+    document.getElementById('galerija3').style.width = "362px";
+    document.getElementById('galerija3').style.height = "177px";
+  }
+
+
+  function two(){
+
+    document.getElementById('galerija2').src = "https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=732&q=80.jpg";
+    document.getElementById('galerija2').style.width = "168px";
+    document.getElementById('galerija2').style.height = "168px";
+    document.getElementById('galerija2').style.transition = "1s"
+    document.getElementById('galerija2').style.position = "relative";
+    document.getElementById('galerija2').style.top = "-400px"; 
+    document.getElementById('galerija2').style.left = "1200px";
+    document.getElementById('galerija2').style.ease = "1s";
+
+    document.getElementById('galerija1').src = "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=449&q=80";
+    document.getElementById('galerija1').style.width = "168px";
+    document.getElementById('galerija1').style.height = "168px";
+    document.getElementById('galerija1').style.transition = "1s"
+    document.getElementById('galerija1').style.position = "relative";
+    document.getElementById('galerija2').style.top = "-410px"; 
+    document.getElementById('galerija2').style.left = "1010px";
+    document.getElementById('galerija1').style.ease = "1s";
+
+
+    document.getElementById('galerija3').src = "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80.jpg";
+    document.getElementById('galerija3').style.width = "362px";
+    document.getElementById('galerija3').style.height = "177px";
+
+  }
+
